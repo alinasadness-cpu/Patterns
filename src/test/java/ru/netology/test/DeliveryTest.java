60da117b52ca2346ce5c915c2d70d07029546ee4
alinasadness-cpu <alina_sadness@inbox.ru>
2026-06-29 17:13:10 +0300

    Update DeliveryTest.java

--- src/test/java/ru/netology/test/DeliveryTest.java
+++ src/test/java/ru/netology/test/DeliveryTest.java
@@ -1,5 +1,6 @@
 package ru.netology.delivery.test;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -7,8 +8,8 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import java.time.Duration;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -35,57 +36,87 @@ public class DeliveryTest {
         SelenideLogger.removeListener("allure");
     }
 
+    private void clearDateField() {
+        $("[data-test-id='date'] input").doubleClick();
+        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
+    }
+
+    private void fillFormAndSubmit(String date) {
+       
+        $("[data-test-id='city'] input").setValue(user.getCity());
+        $("[data-test-id='city'] .menu-item").shouldBe(Condition.visible, Duration.ofSeconds(15)).click();
+
+        clearDateField();
+        $("[data-test-id='date'] input").setValue(date);
+
+        $("[data-test-id='name'] input").setValue(user.getName());
+
+        $("[data-test-id='phone'] input").setValue(user.getPhone());
+
+        
+        $("[data-test-id='agreement']").click();
+
+       
+        $(".button").click();
+    }
+
     @Test
     @DisplayName("Should successfully reschedule meeting when date changed")
     void shouldRescheduleMeetingWhenDateChanged() {
         String firstDate = DataGenerator.generateDate(3);
         String secondDate = DataGenerator.generateDate(7);
 
-    
         fillFormAndSubmit(firstDate);
 
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstDate));
+
+    
+        $("[data-test-id='success-notification'] .icon-button").click();
+
      
-        $("[data-test-id=success-notification]").shouldBe(visible);
-        $("[data-test-id=success-notification] .notification__content")
-                .shouldHave(text("Встреча успешно запланирована на " + firstDate));
+        clearDateField();
+        $("[data-test-id='date'] input").setValue(secondDate);
 
- 
-        $("[data-test-id=success-notification] .icon-button").click();
+    
+        $(".button").click();
 
-  
-        $(".calendar-input__native-control").setValue(secondDate);
+        $("[data-test-id='replan-notification']")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
 
-       
-        $("[data-test-id=replan] .button").click();
-
-      
-        $("[data-test-id=replan-notification]").shouldBe(visible);
-        $("[data-test-id=replan-notification] .notification__content")
-                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
-
-      
         $(byText("Перепланировать")).click();
 
-        $("[data-test-id=success-notification]").shouldBe(visible);
-        $("[data-test-id=success-notification] .notification__content")
-                .shouldHave(text("Встреча успешно запланирована на " + secondDate));
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondDate));
     }
 
     @Test
     @DisplayName("Should show validation messages for invalid data")
     void shouldShowValidationMessagesForInvalidData() {
-       
-        $("[data-test-id=replan] .button").click();
+    
+        $(".button").click();
 
-       
-        $("[data-test-id=city] .input__sub").shouldHave(text("Поле обязательно для заполнения"));
-        
-        
-        $("[data-test-id=date] .input__sub").shouldHave(text("Неверно введена дата"));
-        
-        $("[data-test-id=name] .input__sub").shouldHave(text("Поле обязательно для заполнения"));
-        
-        $("[data-test-id=phone] .input__sub").shouldHave(text("Поле обязательно для заполнения"));
+    
+        $("[data-test-id='city'] .input__sub")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Поле обязательно для заполнения"));
+
+        $("[data-test-id='date'] .input__sub")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Неверно введена дата"));
+
+      
+        $("[data-test-id='name'] .input__sub")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Поле обязательно для заполнения"));
+
+        $("[data-test-id='phone'] .input__sub")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -95,29 +126,32 @@ public class DeliveryTest {
 
         fillFormAndSubmit(meetingDate);
 
-        $("[data-test-id=success-notification]").shouldBe(visible);
-        $("[data-test-id=success-notification] .notification__content")
-                .shouldHave(text("Встреча успешно запланирована на " + meetingDate));
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + meetingDate));
     }
 
-    private void fillFormAndSubmit(String date) {
-      
-        $("[data-test-id=city] input").setValue(user.getCity());
-        $("[data-test-id=city] .menu-item").shouldBe(visible).click();
-
-      
-        $(".calendar-input__native-control").setValue(date);
-
-       
-        $("[data-test-id=name] input").setValue(user.getName());
-
-      
-        $("[data-test-id=phone] input").setValue(user.getPhone());
+    @Test
+    @DisplayName("Should select city from dropdown")
+    void shouldSelectCityFromDropdown() {
+        String city = "Казань";
+        String date = DataGenerator.generateDate(7);
 
         
-        $("[data-test-id=agreement] .checkbox__text").click();
+        $("[data-test-id='city'] input").setValue("Ка");
+        $(".menu-item__control").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $$(".menu-item__control").findBy(Condition.text(city)).click();
 
-      
-        $("[data-test-id=replan] .button").click();
+       
+        clearDateField();
+        $("[data-test-id='date'] input").setValue(date);
+        $("[data-test-id='name'] input").setValue(user.getName());
+        $("[data-test-id='phone'] input").setValue(user.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + date));
     }
 }
